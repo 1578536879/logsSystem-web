aa73adc766abb90e0dc6cc160eb8710ea9bad8ae
yang <1578536879@qq.com>
2019-07-30 21:31:37 +0800

    主页-应用列表

---
+++ project/components/menu.vue
@@ -0,0 +1,81 @@
+<template lang="">
+    <div style='position: absolute; width: 100%; height: 100%;'>
+        <div class = 'top'>
+            <img src = '../images/homepage/home.svg'/>
+            <p>前端日志监控系统</p>
+        </div>
+        <div class='left'>
+            <div class = 'list'>
+                <img src = '../images/homepage/list.svg' />
+                <p>应用列表</p>
+            </div>
+            <div class = 'person'>
+                <img src='../images/homepage/person.svg'/>
+                <p>个人信息</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import router from '../src/router'
+
+
+export default {
+    name: 'home-menu',
+    router,
+    data() {
+        return {
+            
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+        
+    },
+}
+</script>
+<style>
+    *{
+        margin: 0;
+    }
+    .top{
+        background: #F5F5F5;
+        border: 1px solid #DCDCDC;
+    }
+    .top  img{
+        width:2em
+    }
+    .top p{
+        display: inline;
+        font-size: 2em;
+        color: #515151;
+        line-height: 2em;
+    }
+    .left{
+        width: 10em;
+        background: #F5F5F5;
+        height: calc(100% - 4.1em);
+    }
+    .left .list, .left .person{
+        background: white;
+        /* margin-top: 5em; */
+        height: 3em;
+    }
+    .left .list{
+        transform: translateY(5em);
+        margin-bottom: 11em;
+    }
+    .left .list img, .left .person img{
+        width: 1em;
+        padding-left: 1em;
+    }
+    .left .list p, .left .person p{
+        display: inline;
+        font-size: 20px;
+        line-height: 2.5em;
+        color: #515151
+    }
+</style>
---
+++ project/pages/homepage/applicationList.vue
@@ -0,0 +1,188 @@
+<template>
+    <div class="applicationList">
+        <home-menu></home-menu>
+        <!-- <div class="delete">
+            <b-alert show class="alert">
+                <img src="../../images/warning.svg"/>
+                <p style="font-size:30px ">操作提示</p>
+                <p>确认删除吗？</p>
+                <button style="margin-right: 2em;" @click="cancel">取 消</button>
+                <button style="background: #FF7F50;" @click="sureDel">确 认</button>
+            </b-alert>
+
+        </div> -->
+        <b-alert show variant="warning"  class="delete" v-if="del !== -1">
+            <span>你确定要删除此应用吗？</span>
+            <b-button  variant="outline-warning"  @click="sureDel">确 定</b-button>
+            <b-button variant="outline-secondary"  style='margin-right: 4%' @click="cancel">取 消</b-button>
+        </b-alert>
+        <div class="applist">
+            <div v-for="(item, index) in appList" :key="index" class="apps" :style="appStyle[index]" @mouseenter="enter(index)" @mouseleave="leave(index)">
+                <p >{{ item.name }}</p>
+                <span>创建时间：{{item.createTime}}</span> 
+                <div v-if="show === index" class="show">
+                    <button style="margin-right: 10%; background: green;" @click="lookAppInfo(index)">查 看</button>
+                    <button style="background:red" @click="deleteApp(index)">删 除</button>
+                </div>
+            </div>
+            <div style="background: #E3E5E6;" class="add" @click="addApp">
+                <img src = '../../images/homepage/add.svg'/>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import homeMenu from '../../components/menu.vue'
+import router from '../../src/router'
+import send from '../../js/send';
+
+export default {
+    name: 'applicationList',
+    router,
+    components:{
+        homeMenu
+    },
+    data() {
+        return {
+            appInfo: [],
+            appList:[],
+            colors:['#97B7B4', '#929CD2', '#E1B16A', '#CD5C5C', '#20B2AA', '#DB7093', '#7B68EE', '#9EB089', '#DAA520'],
+            appStyle:[],
+            show: -1,
+            del: -1,
+        }
+    },
+    created() {
+        let that = this
+        send.sendMsgGet('http://127.0.0.1:8080/queryAppList', `userId=${that.$route.query.userId}`).then(function(r){
+            if(r.data.code === 100){
+                that.appInfo = r.data.data
+                r.data.data.forEach(ele => {
+                    let time = new Date(parseInt(ele.time))
+                    time = `${time.getFullYear()}.${time.getMonth()+1}.${time.getDate()}` 
+                    let name = ele.name
+                    that.appList.push({
+                        name: name,
+                        createTime: time
+                    })
+                    let index = parseInt(Math.random() * that.colors.length)
+                    that.appStyle.push({background: that.colors[index]})
+                });
+            } else if(r.data.code === 101) {
+                that.appList = []
+            }
+        })
+    },
+    // updated() {
+    //     console.log(this.appList)
+    // },
+    methods: {
+        enter(index){
+            console.log(index)
+            this.appStyle[index] = {
+                background: this.appStyle[index].background,
+                opacity: 0.5
+            }
+            this.show = index
+        },
+        leave(index){
+            this.appStyle[index] = {
+                background: this.appStyle[index].background,
+            }
+            this.show = -1
+        },
+        lookAppInfo(index){
+
+        },
+        deleteApp(index){
+            this.del = index
+            console.log(this.del)
+        },
+        addApp(index){
+
+        },
+        cancel(){
+            this.del = -1
+        },
+        sureDel(){
+            send.sendMessage('delete', 'http://127.0.0.1:8080/deleteApp', {})
+        }
+    },
+}
+</script>
+
+<style>
+    .applicationList{
+        width: 100%;
+    }
+    .applicationList .applist{
+        position: relative;
+        top: 8em;
+        left: 20%;
+        width: 77%;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .applicationList .applist .apps{
+        color: white;
+        text-align: center;
+        padding-top: 1em;
+    }
+    .applicationList .applist .show{
+        width: 100%;
+        opacity: 1;
+        transform: translateY(-85%);
+    }
+    .applicationList .applist .show button{
+        
+        border: none;
+        color: white;
+        width: 35%;
+        height: 1.9em;
+        border-radius: 3px;
+    }
+    .applicationList .applist .apps p{
+        line-height: 2em;
+        -webkit-margin-after: 0;
+    }
+    .applicationList .applist .add, .applicationList .applist .apps{
+        width: 12em;
+        height: 7em;
+        border-radius: 8px;
+        display: inline-block;
+        margin-right: 5em;
+        margin-bottom: 2em;
+    }
+    .applicationList .applist .add img{
+       transform: translateY(1.5em);
+        width: 4em;
+        margin-left: 3.5em;
+    }
+    .delete{
+        top: 6em;
+        left: 20%;
+        width: 70%;
+    }
+    /* .delete .alert{
+        width: 30%;
+        height: 20em;
+        margin: 10em auto;
+        opacity: 1;
+        background: white;
+        text-align: center
+    }
+    .delete .alert img{
+        width:30%
+    } */
+    .delete button{
+        width: 4.5em;
+        float: right;
+        transform: translateY(-0.2em);
+        margin-right: 4%;
+        height: 2em;
+        line-height: 0em;
+    }
+    
+</style>
